10bcc074eb0a858c99d3c6962768ea962c2bc406
Agung Nur Ramadhan <agnuramdan@gmail.com>
2022-09-24 20:40:53 +0700

    feat: dividing learning code to different profile

---
+++ learning.js
@@ -0,0 +1,17 @@
+let resultNumber = "";
+
+for (let number = 1; number <= 20; number++) {
+  // console.log(number);
+  let loopNumber = number;
+
+  if (number % 3 === 0) {
+    loopNumber = "kebab";
+  } else if (number % 4 === 0) {
+    loopNumber = "burger";
+  }
+
+  resultNumber = loopNumber;
+  console.log(resultNumber);
+}
+
+console.log(24 % 4 === 0);
