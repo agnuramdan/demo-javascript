11cf523353aead5122e45419c1855f64621b967d
Agung Nur Ramadhan <agnuramdan@gmail.com>
2022-09-24 20:42:59 +0700

    fix: removing learning code to another file

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 console.log("This is demo for JavaScript");
 
 // for loop
-// change the number that divisble by 3 to kebab and number that divisible by 4 to burger
-
-let resultNumber = "";
-
-for (let number = 1; number <= 20; number++) {
-  // console.log(number);
-  let loopNumber = number;
-
-  if (number % 3 === 0) {
-    loopNumber = "kebab";
-  } else if (number % 4 === 0) {
-    loopNumber = "burger";
-  }
-
-  resultNumber = loopNumber;
-  console.log(resultNumber);
-}
-
-console.log(24 % 4 === 0);
-
 // even and odd number
 
 let evenOddNumberResult = "";
